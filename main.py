8fc86d15bbaa09c4e645dd544990ea2fcacd03d5
Shayaan <Shayaan@macbook-pro.home>
2023-01-21 12:31:46 +0000

    comments

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ class Network:
 	weights: list = field(default_factory=list)
 
 	def setup(self, specifications):
+		# Setup the neural network
+
 		final_spec = len(specifications) - 1
 
 		for i, spec in enumerate(specifications):
@@ -37,11 +39,15 @@ class Network:
 					self.weights[-1].append([randint(0, 5) for _ in range(specifications[i+1].num_nodes)])
 
 	def reset_network(self):
+		# Reset the node values
+
 		for i in range(len(self.layers)):
 			for j in range(len(self.layers[i])):
 				self.layers[i][j].value = 0
 
 	def traverse(self, input) -> list:
+		# Pass the input through the network and return the output
+
 		self.reset_network()
 
 		if len(input) != len(self.layers[0]):
@@ -62,18 +68,24 @@ class Network:
 		return [node.value for node in self.layers[-1]]
 
 	def calculate_loss(self, x, y) -> int:
+		# Calculate the means squared error for a given pair of data
+
 		predicted = sum(self.traverse([x]))
 		actual = sum([y])
 
 		return (predicted - actual) ** 2
 
 	def calculate_cost(self, x_data, y_data) -> int:
+		# Calculate the sum of mse for all training data
+
 		cost = 0
 		for i, x in enumerate(x_data):
 			cost += self.calculate_loss(x, y_data[i])
 		return cost
 
 	def derivative(self, x_data, y_data, h=0.000001, weight=None, bias=None) -> int:
+		# Calculate the partial derivative of a given weight or bias with respect to the cost
+
 		a = self.calculate_cost(x_data, y_data)
 		
 		if bias is None:
